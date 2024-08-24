5f9d2622c86323af0fc594284c7b4a50b787c6fd
VladPySuper <121505989+Vool14@users.noreply.github.com>
2024-08-24 11:04:10 +0500

    Batch release

---
+++ Random.bat
@@ -0,0 +1,12 @@
+@echo off
+color 3a
+echo Starting...
+echo Random, version 1.0
+echo Developed by VladPySuper (github nick vool14), 2024
+echo Press and key to generate random number...
+PAUSE
+:A
+color 3b
+echo Your random number: %random%
+PAUSE
+goto A
